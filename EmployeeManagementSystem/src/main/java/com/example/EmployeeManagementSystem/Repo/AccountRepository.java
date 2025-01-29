b817edbafc6ab4315e85cf5473b6a2b098ed22e4
Hirun Senarathna <hirunsenarathna@users.noreply.github.com>
2025-01-29 19:09:15 +0530

    removed unwanted imports

--- EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/Repo/AccountRepository.java
+++ EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/Repo/AccountRepository.java
@@ -1,8 +1,6 @@
 package com.example.EmployeeManagementSystem.Repo;
 
 import com.example.EmployeeManagementSystem.Entity.Account;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
