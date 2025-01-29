217db7ad9e52d450ea0e8c6897b8982eb6399a4e
Hirun Senarathna <hirunsenarathna@users.noreply.github.com>
2025-01-29 19:06:35 +0530

    validated email

--- EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/Repo/AccountRepository.java
+++ EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/Repo/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.example.EmployeeManagementSystem.Repo;
 
 import com.example.EmployeeManagementSystem.Entity.Account;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,5 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
     Optional<Account> findByEmail(String email);
 
 
+    boolean existsByEmail( String email);
 }
